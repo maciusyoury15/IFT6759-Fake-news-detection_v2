b174fe6bdbdb5197d19cdce66d1cbf78a4d2ba44
Claire Shaw <claireshaw17@gmail.com>
2025-03-08 21:29:07 -0500

    Modif structure

---
+++ fake_news_detection/src/__init__.py
@@ -0,0 +1,6 @@
+"""Source code of your project"""
+from src.utils import *
+from src.data_fetch import *
+from src.images_fetch import *
+from src.process import *
+from src.train_model import *
--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Source code of your project"""
-from src.utils import *
-from src.data_fetch import *
-from src.images_fetch import *
-from src.process import *
-from src.train_model import *
